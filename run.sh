ff3c6202a08e33d809b5e6bff4ae46294b186129
Raphael Beer <raphael.beer@protonmail.com>
2019-09-03 18:07:30 +0200

    Add: docker setup

--- run.sh
+++ run.sh
@@ -13,15 +13,6 @@ if [ ! -f $EXPECTED_ENV_FILE ]; then
 fi
 echo "Using configuration from: $EXPECTED_ENV_FILE"
 source $EXPECTED_ENV_FILE
+echo "Listening on: $PORT"
 
-echo "Starting server..."
-python3 ./backend.py \
-  --account-file $ACCOUNT_FILE \
-  --cookie-dir $COOKIE_DIR \
-  --log $LOG_FILE \
-  --debug $DEBUG_FILE \
-  --port "$PORT" \
-  --mongo-host $MONGO_HOST \
-  --mongo-port $MONGO_PORT \
-  --mongo-db $MONGO_DB \
-  --twitter-auth-key $TWITTER_AUTH_KEY
+PORT=$PORT EXPECTED_ENV_FILE=$EXPECTED_ENV_FILE docker-compose -f docker-compose.yml up
